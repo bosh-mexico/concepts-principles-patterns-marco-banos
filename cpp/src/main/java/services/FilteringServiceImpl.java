464085993d390bdc03b9b566eb96f441a496ba17
marco-banos <bma2que@bosch.com>
2025-09-24 09:22:46 -0600

    using charAt
    using charAt

--- cpp/src/main/java/services/FilteringServiceImpl.java
+++ cpp/src/main/java/services/FilteringServiceImpl.java
@@ -6,9 +6,9 @@ import java.util.stream.Collectors;
 public class FilteringServiceImpl implements FilteringService {
 
     public List<String> Filter(List<String> list) {
-        // Filtering
+        // Filtering with first char
         List<String> result = list.stream()
-                .filter(s -> s.startsWith("M"))
+                .filter(s -> !s.isEmpty() && s.charAt(0) == 'M')
                 .collect(Collectors.toList());
 
         return result;
